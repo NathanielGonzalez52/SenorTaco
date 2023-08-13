205e36d2d0556a6e4803c5ff99932d1622895149
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2023-08-13 10:17:30 -0700

    Add css to about section and add images

--- src/components/About.js
+++ src/components/About.js
@@ -3,23 +3,26 @@ import "./About.css";
 
 function About() {
   return (
+    <>
+    <h1 className="welcome">Bienvenidos to Senor Taco!</h1>
     <div className="about">
+
       <div className = "about-text">
-      <h1>Bienvenidos to Senor Taco!</h1>
-      <h3>We are a Mexican restaurant in Cypress, CA 
+      <h5>We are a Mexican restaurant in Cypress, CA 
       dedicated to authenic Mexcan flavor and authenticity! Our menu
       has a wide range of options from quesadllas, tortas, caldos, breakfast,
       hamburgers, and much more. We are located across the street from Cypress
       college and are next to Wisdom Mission School. We offer online 
       and delivery options. Thank you for your time and we hope to 
-      see you soon!</h3>
+      see you soon!</h5>
       </div>
       <div className="pics">
-        <img src="../../images/taco.jpg"></img>
-        <img src="../../images/fries.jpg"></img>
-        <img src="../../images/quesa.jpg"></img>
+        <img className="food" src="../../images/taco.jpg"></img>
+        <img className="food" src="../../images/fries.jpg"></img>
+        <img className="food" src="../../images/quesa.jpg"></img>
       </div>
     </div>
+    </>
   )
 }
 
