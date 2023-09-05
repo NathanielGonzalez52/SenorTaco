cdf6562bfbd63b30855b082614f309b9b18a5eda
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2023-09-05 00:11:22 -0700

    Fix responsiveness for hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,9 +5,9 @@ import "../App.css";
 function HeroSection() {
     return (
         <div className = "hero-container">
-        <img className = "hero" src="../../images/taquitos.jpg"></img>
-        <img className = "hero" src="../../images/wet_burrito3.jpg"></img>
-        <img className = "hero" src="../../images/tacos_display.jpg"></img>
+        <img className = "hero first" src="../../images/taquitos.jpg"></img>
+        <img className = "hero second" src="../../images/wet_burrito3.jpg"></img>
+        <img className = "hero third" src="../../images/taquito_plate.jpg"></img>
 
         {/* background: url("../../public/images/taquitos.jpg") left, url("../../public/images/wet_burrito3.jpg") center, url("../../public/images/tacos_display.jpg") right; */}
         </div>
